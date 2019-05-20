630be78fcaf8e5176752dd83f3823fe2086bc6fd
Jordan Hendrickson <jordanhendrickson@Jordans-MacBook-Pro.local>
2019-05-20 10:14:52 -0600

    first commit

---
+++ Playlist-OBJC/PlaylistOBJC/PlaylistOBJC/PlaylistOBJC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlaylistOBJC
+//
+//  Created by Jordan Hendrickson on 5/20/19.
+//  Copyright © 2019 Jordan Hendrickson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Playlist-OBJC/PlaylistOBJC/PlaylistOBJC/PlaylistOBJC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PlaylistOBJC
+//
+//  Created by Jordan Hendrickson on 5/20/19.
+//  Copyright © 2019 Jordan Hendrickson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
